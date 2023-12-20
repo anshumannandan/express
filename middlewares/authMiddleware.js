29d8d8ac3a3823eb920f40cc11eb0d709fd9c92c
anshumannandan <anshuman21xxx034@akgec.ac.in>
2023-12-20 17:19:42 +0530

    fix: token authentication

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 
 const authenticate = async (req, res, next) => {
-  const token = req.header('Authorization');
+  const token = req.header('Authorization').replace('Bearer ', '');
 
   if (!token) {
     return res.status(401).json({ message: 'Unauthorized' });
